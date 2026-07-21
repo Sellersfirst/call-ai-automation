d7518e331204e65bd86e652d9d1f05a96227fb93
mhassamzahid <hassam1_zahid@hotmail.com>
2026-07-21 14:06:17 +0500

    updated code to use different api when rubrics prompt is used

--- config/config.py
+++ config/config.py
@@ -18,6 +18,7 @@ class Settings:
     ELEVEN_LABS_KEY: str = get_env("ELEVEN_LABS_KEY")
     ELEVEN_AGENT_ID: str = get_env("ELEVEN_AGENT_ID")
     ANTHROPIC_API_KEY: str = get_env("ANTHROPIC_API_KEY")
+    ANTHROPIC_API_KEY_RUBRIC: str = get_env("ANTHROPIC_API_KEY_RUBRIC", required=False, default=None)
     ELEVENLABS_URL = "https://api.elevenlabs.io/v1/convai/twilio/outbound-call"
     ALAB_SPREADSHEET_NAME="ALab (dialforce) - Nationwide High Intent First only- 03.09.26"
     ALAB_WORKSHEET_NAME="ALab (dialforce) - Nationwide High Intent First only- 03.09.26"
@@ -43,5 +44,6 @@ ALAB_WORKSHEET_NAME = settings.ALAB_WORKSHEET_NAME
 MAPPER_SHEET_NAME = settings.MAPPER_SHEET_NAME
 MAPPER_WORKSHEET_NAME = settings.MAPPER_WORKSHEET_NAME
 ANTHROPIC_API_KEY = settings.ANTHROPIC_API_KEY
+ANTHROPIC_API_KEY_RUBRIC = settings.ANTHROPIC_API_KEY_RUBRIC or settings.ANTHROPIC_API_KEY
 # DEFAULT_PHONE = 'phnum_7801k60w0n6vecav04d878ej4g7x'
 DEFAULT_PHONE = 'phnum_3401knvxn7egetn9vs54vkc4b4hp'
